648da7e9374b45c1203bef68ea114509959159a9
artem-chunikhin <artem.chunikhin@sitecore.net>
2021-06-10 19:42:49 +0300

    Initial

---
+++ src/Sitecore.Support.479210/QueryableDatasource.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.Data.Items;
+using Sitecore.DependencyInjection;
+using Sitecore.Diagnostics;
+using Sitecore.Pipelines.ResolveRenderingDatasource;
+using Sitecore.Text;
+using Sitecore.XA.Foundation.Abstractions;
+using Sitecore.XA.Foundation.Multisite.Extensions;
+using Sitecore.XA.Foundation.SitecoreExtensions.Extensions;
+using Sitecore.XA.Foundation.TokenResolution;
+
+namespace Sitecore.XA.Foundation.LocalDatasources.Pipelines.ResolveRenderingDatasource
+{
+    public class QueryableDatasource
+    {
+        public void Process(ResolveRenderingDatasourceArgs args)
+        {
+            Assert.IsNotNull(args, "args");
+
+            if (args.Datasource.StartsWith("query:", StringComparison.InvariantCultureIgnoreCase))
+            {
+                Item contextItem = args.GetContextItem();
+                if (contextItem != null)
+                {
+                    string query = TokenResolver.Resolve(args.Datasource, contextItem);
+                    Item[] queryItems = contextItem.SelectItemsWithLanguage(query);
+                    ListString itemIds = new ListString(queryItems.Select(i => i.ID.ToString()).ToList());
+                    args.Datasource = itemIds.ToString();
+                    return;
+                }
+
+                args.Datasource = string.Empty;
+            }
+        }
+    }
+}
